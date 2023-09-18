fe9bd84ef2ec16b5d50a165db173ef645de327cf
Mathieu MAS <34915773+mathmas@users.noreply.github.com>
2023-09-18 15:38:38 -0400

    Add Car

---
+++ Assets/Scripts/Car/CarBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CarBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
